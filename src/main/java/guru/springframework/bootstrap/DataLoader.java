5beef7fa00fbf11f5d9af366e9eee49e344abfab
JustinSHong <jhong2009@gmail.com>
2020-05-22 15:58:24 -0700

    create guac recipe object

--- src/main/java/guru/springframework/bootstrap/DataLoader.java
+++ src/main/java/guru/springframework/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.bootstrap;
 
 import guru.springframework.domain.Category;
+import guru.springframework.domain.Difficulty;
 import guru.springframework.domain.Recipe;
 import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositories.CategoryRepository;
@@ -112,7 +113,28 @@ public class DataLoader implements CommandLineRunner {
 
 
         // create guacamole recipe
-
+        Recipe guacamoleRecipe = new Recipe();
+        guacamoleRecipe.setDescription("Perfect Guacamole");
+        guacamoleRecipe.setPrepTime(10);
+        guacamoleRecipe.setCookTime(10);
+        guacamoleRecipe.setServings(4);
+        guacamoleRecipe.setSource("");
+        guacamoleRecipe.setUrl("https://www.simplyrecipes.com/recipes/perfect_guacamole/");
+        guacamoleRecipe.setDirections("1. Cut the avocado, remove flesh: Cut the avocados in half. Remove the pit. Score the inside of the avocado with a blunt knife and scoop out the flesh with a spoon. (See How to Cut and Peel an Avocado.) Place in a bowl.\n"
+                + "2. Mash with a fork: Using a fork, roughly mash the avocado. (Don't overdo it! The guacamole should be a little chunky.) \n"
+                + "3. Add salt, lime juice, and the rest: Sprinkle with salt and lime (or lemon) juice. The acid in the lime juice will provide some balance to the richness of the avocado and will help delay the avocados from turning brown.\n" +
+                        "\n" +
+                        "Add the chopped onion, cilantro, black pepper, and chiles. Chili peppers vary individually in their hotness. So, start with a half of one chili pepper and add to the guacamole to your desired degree of hotness.\n" +
+                        "\n" +
+                        "Remember that much of this is done to taste because of the variability in the fresh ingredients. Start with this recipe and adjust to your taste.\n" +
+                        "\n" +
+                        "Chilling tomatoes hurts their flavor, so if you want to add chopped tomato to your guacamole, add it just before serving. \n"
+                + "4. Serve: Serve immediately, or if making a few hours ahead, place plastic wrap on the surface of the guacamole and press down to cover it and to prevent air reaching it. (The oxygen in the air causes oxidation which will turn the guacamole brown.) Refrigerate until ready to serve."
+        );
+//        guacamoleRecipe.setIngredients();
+//        guacamoleRecipe.setImage();
+        guacamoleRecipe.setDifficulty(Difficulty.EASY);
+//        guacamoleRecipe.setNotes();
 
         // create spicy chicken taco recipe
 
