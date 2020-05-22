1e2119de54bf6a1976dee0e9938f9dfeea2e4e9a
JustinSHong <jhong2009@gmail.com>
2020-05-22 15:48:19 -0700

    get units of measure

--- src/main/java/guru/springframework/bootstrap/DataLoader.java
+++ src/main/java/guru/springframework/bootstrap/DataLoader.java
@@ -1,4 +1,128 @@
 package guru.springframework.bootstrap;
 
-public class DataLoader {
+import guru.springframework.domain.Category;
+import guru.springframework.domain.Recipe;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+    private final RecipeRepository recipeRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public DataLoader(CategoryRepository categoryRepository, RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.recipeRepository = recipeRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        getRecipes();
+    }
+
+    private List<Recipe> getRecipes() {
+        List<Recipe> recipes = new ArrayList<>(2);
+
+        // check units of measure are present in the database
+        Optional<UnitOfMeasure> teaSpoonOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        if (!teaSpoonOptional.isPresent()) {
+            throw new RuntimeException("Expected Unit of Measure not found");
+        }
+
+        Optional<UnitOfMeasure> tableSpoonOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+        if (!tableSpoonOptional.isPresent()) {
+            throw new RuntimeException("Expected Unit of Measure not found");
+        }
+
+        Optional<UnitOfMeasure> cupOptional = unitOfMeasureRepository.findByDescription("Cup");
+        if (!cupOptional.isPresent()) {
+            throw new RuntimeException("Expected Unit of Measure not found");
+        }
+
+        Optional<UnitOfMeasure> pinchOptional = unitOfMeasureRepository.findByDescription("Pinch");
+        if (!pinchOptional.isPresent()) {
+            throw new RuntimeException("Expected Unit of Measure not found");
+        }
+
+        Optional<UnitOfMeasure> ounceOptional = unitOfMeasureRepository.findByDescription("Ounce");
+        if (!ounceOptional.isPresent()) {
+            throw new RuntimeException("Expected Unit of Measure not found");
+        }
+
+        Optional<UnitOfMeasure> dashOptional = unitOfMeasureRepository.findByDescription("Dash");
+        if (!dashOptional.isPresent()) {
+            throw new RuntimeException("Expected Unit of Measure not found");
+        }
+
+        Optional<UnitOfMeasure> pintOptional = unitOfMeasureRepository.findByDescription("Pint");
+        if (!pintOptional.isPresent()) {
+            throw new RuntimeException("Expected Unit of Measure not found");
+        }
+
+        Optional<UnitOfMeasure> eachOptional = unitOfMeasureRepository.findByDescription("Each");
+        if (!eachOptional.isPresent()) {
+            throw new RuntimeException("Expected Unit of Measure not found");
+        }
+
+        // get optionals
+        UnitOfMeasure teaSpoonUom = teaSpoonOptional.get();
+        UnitOfMeasure tableSpoonUom = tableSpoonOptional.get();
+        UnitOfMeasure cupUom = cupOptional.get();
+        UnitOfMeasure pinchUom = pinchOptional.get();
+        UnitOfMeasure ounceUom = ounceOptional.get();
+        UnitOfMeasure dashUom = dashOptional.get();
+
+        // check categories are present in the database
+        Optional<Category> americanCategoryOptional = categoryRepository.findByDescription("American");
+        if (!americanCategoryOptional.isPresent()) {
+            throw new RuntimeException("Expected Category not found");
+        }
+
+        Optional<Category> italianCategoryOptional = categoryRepository.findByDescription("Italian");
+        if (!italianCategoryOptional.isPresent()) {
+            throw new RuntimeException("Expected Category not found");
+        }
+
+        Optional<Category> mexicanCategoryOptional = categoryRepository.findByDescription("Mexican");
+        if (!mexicanCategoryOptional.isPresent()) {
+            throw new RuntimeException("Expected Category not found");
+        }
+
+        Optional<Category> fastFoodOptional = categoryRepository.findByDescription("Fast Food");
+        if (!fastFoodOptional.isPresent()) {
+            throw new RuntimeException("Expected Category not found");
+        }
+
+        // get categories
+        Category americanCategory = americanCategoryOptional.get();
+        Category mexicanCategory = mexicanCategoryOptional.get();
+        Category italianCategory = italianCategoryOptional.get();
+        Category fastFoodCategory = fastFoodOptional.get();
+
+
+        // create guacamole recipe
+
+
+        // create spicy chicken taco recipe
+
+
+
+        // add recipes to the list of recipes
+
+
+
+
+        return recipes;
+    }
 }
